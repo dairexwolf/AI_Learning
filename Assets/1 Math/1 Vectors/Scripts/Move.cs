eb3f2a25df04a8ed2164bfa5231f8130d7b1c62c
dairexwolf <93929534+dairexwolf@users.noreply.github.com>
2024-12-18 15:42:57 +0700

    Repeating and sorting
    Repeating and sorting lessons 1 and 2

---
+++ Assets/1 Math/1 Vectors/Scripts/Move.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 3f;
+
+    void Start()
+    {
+
+        direction = goal.transform.position - this.transform.position;
+        direction.y = 0;
+        // transform.position += direction; // Это тоже самое, что и translate
+
+    }
+
+    private void LateUpdate() 
+    {
+        this.transform.LookAt(goal.transform.position);
+        direction = goal.transform.position - this.transform.position;
+        direction.y = 0;
+        if (direction.magnitude > 2f)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position += velocity;
+        }
+    }
+}
