50fc3f8294d863cc386b1338ae1682a210a159c3
dairexwolf <93929534+dairexwolf@users.noreply.github.com>
2025-01-13 17:29:06 +0700

    Add new behaviors

--- Assets/7 AutonomouslyMovingAgents/Scripts/Bot.cs
+++ Assets/7 AutonomouslyMovingAgents/Scripts/Bot.cs
@@ -10,6 +10,24 @@ public class Bot : MonoBehaviour
     public GameObject target;
     DrivePoliceman targetDrive;
 
+    // Переменные для функции Wonder
+    /// <summary>
+    /// Радиус круга
+    /// </summary>
+    public float wanderRadius = 10f;
+    /// <summary>
+    /// Дистанция до середины вооброжаемого круга
+    /// </summary>
+    public float wanderDistance = 20f;
+    /// <summary>
+    /// Значение, которая устанавливает, насколько сильно НИП будет поворачиваться 
+    /// </summary>
+    public float wanderJitter = 1f;
+    /// <summary>
+    /// Место, куда пойдет бродить НИП
+    /// </summary>
+    Vector3 wanderTarget = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +38,13 @@ public class Bot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Evade();
+        Wander();
     }
 
-    // Преследование цели по заданой координате
+    /// <summary>
+    /// Преследование цели по заданой координате
+    /// </summary>
+    /// <param name="location"></param>
     void Seek(Vector3 location)
     {
         agent.SetDestination(location);
@@ -37,7 +58,9 @@ public class Bot : MonoBehaviour
         agent.SetDestination(this.transform.position - fleeVector);
     }
 
-    // Преследование цели
+    /// <summary>
+    /// Преследование цели
+    /// </summary>
     void Pursue()
     {
         // Дистанция до цели
@@ -63,7 +86,9 @@ public class Bot : MonoBehaviour
         Seek(target.transform.position + target.transform.forward * lookAhead);
     }
 
-    // Уклонение от цели
+    /// <summary>
+    /// Уклонение от цели
+    /// </summary>
     void Evade()
     {
         // Дистанция до цели
@@ -87,6 +112,56 @@ public class Bot : MonoBehaviour
         float lookAhead = targetDir.magnitude / (agent.speed + targetDrive.currentSpeed);
         // Уход в сторону, используя предсказание
         Flee(target.transform.position + target.transform.forward * lookAhead * 2);
+    }
+
+    /// <summary>
+    /// Брождение НИПа
+    /// </summary>
+    void Wander()
+    {
+        // Устанавливаем направление, куда пойдет НИП. Делается это при помощи рандома
+        wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter, 0, Random.Range(-1.0f, 1.0f) * wanderJitter);
+        // Нормализуем магнитуду вектора
+        wanderTarget.Normalize();
+        // И устанавливаем в радиус круга
+        wanderTarget *= wanderRadius;
+
+        // Через какое расстояние будет центр круга в локальных координатах
+        Vector3 targetLocal = wanderTarget + new Vector3(0, 0, wanderDistance);
+        // Переводим их в глобальные координаты, чтобы послать их в NavMeshAgent
+        Vector3 targetWorld = gameObject.transform.InverseTransformVector(targetLocal);
+
+        // И ведем туда НИПа
+        Seek(targetWorld);
+    }
+
+    /// <summary>
+    /// Попытка НИПа спрятаться
+    /// </summary> 
+    void Hide()
+    {
+        // Инициализация переменных
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+
+        // Получение всех объектов, за которыми можно спрятаться
+        int len = World.Instance.GetHidingSpots().Length;
+        
+        // Перебор всех препятсвий и выбор ближайшего
+        for (int i=0;i<len; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            // Умножаем на 5, чтобы вынести координату за препятствие
+            Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 8;
+
+            if(Vector3.Distance(this.transform.position, hidePos)<dist)
+            {
+                chosenSpot = hidePos;
+                dist = Vector3.Distance(this.transform.position, hidePos);
+            }
+        }
+
+        Seek(chosenSpot);
     }
 
 }
