b952da3e36b0f793b3a64ba543855a12e593fe3d
dairexwolf <93929534+dairexwolf@users.noreply.github.com>
2025-02-12 17:18:21 +0700

    Start do GPlanner

--- Assets/9 GOAP/1 Hospital/Scripts/GAgent.cs
+++ Assets/9 GOAP/1 Hospital/Scripts/GAgent.cs
@@ -2,16 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class SubGoal
+{
+    public Dictionary<string, int> sGoals;
+    public bool remove;
+
+    public SubGoal(string str, int i, bool rem)
+    {
+        sGoals = new Dictionary<string, int>();
+        sGoals.Add(str, i);
+        remove = rem;
+    }
+}
+
 public class GAgent : MonoBehaviour
 {
+    public List<GAction> actions = new List<GAction>();
+    public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
+
+    GPlanner planner;
+    Queue<GAction> actionQueue;
+    public GAction currentAction;
+    public SubGoal currentGoal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GAction[] acts = this.GetComponents<GAction>();
+        foreach(GAction a in acts)
+        {
+            actions.Add(a);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         
     }
